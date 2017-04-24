543427de12dd6e6db5f48c5a26868dfbb29ebc61
Kongpon Charanwattanakit <kongpon.c@ku.th>
2017-04-24 08:12:50 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'application/activity1'
+  get 'application/activity1', to:'application#activity1'
   root 'application#home'
 end
