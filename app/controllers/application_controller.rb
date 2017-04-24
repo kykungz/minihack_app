db2edf7f23764a546e1928d0968eb23b3468d04b
Kongpon Charanwattanakit <kongpon.c@ku.th>
2017-04-24 07:50:14 +0000

    add root router

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def home
+      render html: "Welcome to Kongpon, Thitiwat and Worawat Mini-Hack website"
+  end
 end
